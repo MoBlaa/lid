eda357d3c3254c4f5c5681d90c4956c5a3121724
Moritz Hepp <moritz.hepp@gmx.de>
2020-03-18 10:28:57 +0100

    using flutter web support instead of angulardart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+
+import 'bloc/startpage.dart';
 
 void main() => runApp(MyApp());
 
